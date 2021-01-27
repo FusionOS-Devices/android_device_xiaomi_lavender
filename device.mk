96d901d72ad8eeeedc07951864f13fcec4758a0b
SparXFusion <s2234nadar@gmail.com>
2021-01-27 17:24:30 +0530

    lavender: bringup FusionOS
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -21,8 +21,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-fuse
 
 # RRO configuration
 TARGET_USES_RRO := true
